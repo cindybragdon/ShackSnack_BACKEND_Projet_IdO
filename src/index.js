ebb3edad9fdcdfaef14007ff028187a778f5e23b
olivierpoirier <117477661+olivierpoirier@users.noreply.github.com>
2025-02-19 16:39:10 -0500

    ajout HTTPS

--- src/index.js
+++ src/index.js
@@ -1,59 +1,54 @@
 import express from 'express';
 import cors from 'cors';
+import fs from 'fs';
+import https from 'https';
+import os from 'os';
 import { config } from './config/config.js';
 import userRoutes from './routes/user.routes.js';
 import feedingLogsRoutes from './routes/feedingLog.routes.js';
 import { errorMiddleware } from './middlewares/errorMiddleware.js';
 import { connectToMongoDatabase } from './utils/mongo.connectToDatabase.js';
 
-// import fs from 'fs';
-// import https from 'https';
-
-
-
 const app = express();
-const port = config.port;
-
+const port = config.port || 443;
 
 app.use(cors());
 
-/*
 const options = {
   key: fs.readFileSync('./src/keys/key.pem'),
-  cert: fs.readFileSync('./src/keys/cert.pem')
+  cert: fs.readFileSync('./src/keys/cert.pem'),
 };
 
-*/
-
-
 app.use('/', userRoutes);
-
 app.use('/', feedingLogsRoutes);
-
-
 app.use(errorMiddleware);
 
 app.get('/', (req, res) => {
   res.send('Welcome to SnackShack API');
 });
 
+// Fonction pour récupérer l'adresse IPv4 locale
+const getLocalIPv4 = () => {
+  const interfaces = os.networkInterfaces();
+  for (const iface of Object.values(interfaces)) {
+    for (const details of iface) {
+      if (details.family === 'IPv4' && !details.internal) {
+        return details.address;
+      }
+    }
+  }
+  return 'localhost';
+};
 
-const server = app.listen(port, async () => {
-  await connectToMongoDatabase(config.DB_PROD_IDO_PROJET)
-  console.log("Serveur prod started");
-  console.log(`Server is running on port http://localhost:${port}`);
+const ipAddress = getLocalIPv4();
+
+const server = https.createServer(options, app).listen(port, async () => {
+  await connectToMongoDatabase(config.DB_PROD_IDO_PROJET);
+  console.log("✅ Serveur HTTPS démarré !");
+  console.log(`🌍 Accessible à :`);
+  console.log(`   🔹 https://localhost:${port}`);
+  console.log(`   🔹 https://${ipAddress}:${port}`);
+  console.log("➡️  Veuillez accéder à l'une de ces pages pour accepter l'accès")
 });
 
-/*
-    https.createServer(options, app).listen(port, async () => {
-      await connectToMongoDatabase(config.DB_PROD_URI_FINAL)
-      //fetchAllData(config.databaseFetchUrl);
-      console.log("Serveur prod started");
-      console.log(`Server is running on port https://localhost:${port}`);
-      }).on('error', () => {
-        console.error('HTTPS server error:', error);
-      });
-      */
-//}
-
-export {server, app};
+export { server, app };
