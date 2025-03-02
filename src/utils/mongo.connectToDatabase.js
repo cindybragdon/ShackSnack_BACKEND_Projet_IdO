c4f1fc27f3ee7113b26cd029c550c4f9aeac7dae
olivierpoirier <117477661+olivierpoirier@users.noreply.github.com>
2025-03-02 16:25:34 -0500

    enleve message

--- src/utils/mongo.connectToDatabase.js
+++ src/utils/mongo.connectToDatabase.js
@@ -5,7 +5,7 @@ import mongoose from "mongoose"
 export const connectToMongoDatabase = async (database) => {
     try {
         await mongoose.connect(database);
-        console.log(`Connected to the database to URI ${database}`);
+        console.log(`Connected to Mongo Database!`);
         
     } catch(error) {
         console.error("Cannot connect to Mongo DB : ", error)
