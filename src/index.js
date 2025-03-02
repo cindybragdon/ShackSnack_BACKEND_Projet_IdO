bd962bd3f72b5dd1e909b41b6c0c8daac2c31d9b
olivierpoirier <117477661+olivierpoirier@users.noreply.github.com>
2025-03-02 16:04:14 -0500

    enleve modifs deploy

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import cors from 'cors';
+import fs from 'fs';
+import https from 'https';
 import os from 'os';
 import { config } from './config/config.js';
 import userRoutes from './routes/user.routes.js';
@@ -8,13 +10,15 @@ import { errorMiddleware } from './middlewares/errorMiddleware.js';
 import { connectToMongoDatabase } from './utils/mongo.connectToDatabase.js';
 
 const app = express();
-const port = process.env.PORT || 4837; // Render dynamically assigns a port
+const port = config.port || 443;
 
-// Allow CORS for your frontend (change to your actual frontend URL in production)
-app.use(cors({ origin: '*' })); 
+app.use(cors());
 
-// Middleware and routes
-app.use(express.json());
+const options = {
+  key: fs.readFileSync('./src/keys/key.pem'),
+  cert: fs.readFileSync('./src/keys/cert.pem'),
+};
+
 app.use('/', userRoutes);
 app.use('/', feedingLogsRoutes);
 app.use(errorMiddleware);
@@ -23,10 +27,39 @@ app.get('/', (req, res) => {
   res.send('Welcome to SnackShack API 🦮🐈🐕🐈‍⬛🐕‍🦺');
 });
 
-// Connect to MongoDB and start the server
+// Fonction pour récupérer l'adresse IPv4 locale
+const getLocalIPv4 = () => {
+  const interfaces = os.networkInterfaces();
+  for (const iface of Object.values(interfaces)) {
+    for (const details of iface) {
+      if (details.family === 'IPv4' && !details.internal) {
+        return details.address;
+      }
+    }
+  }
+  return 'localhost';
+};
+
+const ipAddress = getLocalIPv4();
+
+
 const server = app.listen(port, async () => {
   await connectToMongoDatabase(config.DB_PROD_IDO_PROJET);
-  console.log(`✅ Server running on port ${port}`);
-});
+  console.log("✅ Serveur HTTP démarré !");
+  console.log(`🌍 Accessible à :`);
+  console.log(`   🔹 http://localhost:${port}`);
+  console.log(`   🔹 http://${ipAddress}:${port}`);
+  console.log("➡️  Veuillez accéder à l'une de ces pages pour accepter l'accès");
+})
 
+/*
+const server = https.createServer(options, app).listen(port, async () => {
+  await connectToMongoDatabase(config.DB_PROD_IDO_PROJET);
+  console.log("✅ Serveur HTTPS démarré !");
+  console.log(`🌍 Accessible à :`);
+  console.log(`   🔹 https://localhost:${port}`);
+  console.log(`   🔹 https://${ipAddress}:${port}`);
+  console.log("➡️  Veuillez accéder à l'une de ces pages pour accepter l'accès")
+});
+*/
 export { server, app };
